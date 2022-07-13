5822bcee4003f4bc306438577fdef54d292b685b
songsub-cha <52591516+songsub-cha@users.noreply.github.com>
2022-07-13 14:48:03 +0900

    added source files

---
+++ Customer.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Customer {
+	private String name;
+
+	private List<Rental> rentals = new ArrayList<Rental>();
+
+	public Customer(String name) {
+		this.setName(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Rental> getRentals() {
+		return rentals;
+	}
+
+	public void setRentals(List<Rental> rentals) {
+		this.rentals = rentals;
+	}
+
+	public void addRental(Rental rental) {
+		rentals.add(rental);
+
+	}
+    // Long Method - SRP violation
+	// Feature envy - divergent change
+	public String getReport() {
+		String result = "Customer Report for " + getName() + "\n";
+
+		List<Rental> rentals = getRentals();
+
+		double totalCharge = 0;
+		int totalPoint = 0;
+
+		for (Rental each : rentals) {
+			double eachCharge = 0;
+			int eachPoint = 0 ;
+			int daysRented = 0;
+
+			// duplication
+			if (each.getStatus() == 1) { // returned Video
+				long diff = each.getReturnDate().getTime() - each.getRentDate().getTime();
+				// duplication
+				daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+			} else { // not yet returned
+				long diff = new Date().getTime() - each.getRentDate().getTime();
+				daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+			}
+
+			// enum
+			switch (each.getVideo().getPriceCode()) {
+			case Video.REGULAR:
+				// magic number
+				eachCharge += 2;
+				if (daysRented > 2)
+					eachCharge += (daysRented - 2) * 1.5;
+				break;
+			case Video.NEW_RELEASE:
+				eachCharge = daysRented * 3;
+				break;
+			}
+	        // accumulation
+			eachPoint++;
+
+			if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) )
+				eachPoint++;
+
+			if ( daysRented > each.getDaysRentedLimit() )
+				eachPoint -= Math.min(eachPoint, each.getVideo().getLateReturnPointPenalty()) ;
+
+			// report generation
+			result += "\t" + each.getVideo().getTitle() + "\tDays rented: " + daysRented + "\tCharge: " + eachCharge
+					+ "\tPoint: " + eachPoint + "\n";
+
+			// seperate change & point
+			totalCharge += eachCharge;
+
+			totalPoint += eachPoint ;
+		}
+
+		result += "Total charge: " + totalCharge + "\tTotal Point:" + totalPoint + "\n";
+
+		//
+		if ( totalPoint >= 10 ) {
+			System.out.println("Congrat! You earned one free coupon");
+		}
+		if ( totalPoint >= 30 ) {
+			System.out.println("Congrat! You earned two free coupon");
+		}
+		return result ;
+	}
+}
