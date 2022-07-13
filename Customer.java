563c008b6d70341998f098dfe3279151c8fa59ae
songsub-cha <52591516+songsub-cha@users.noreply.github.com>
2022-07-13 16:09:52 +0900

    Long Method
    Customer.java에서 getReport() 를 responsibility에 따라 extract method를 수행
    - calculateChage() : Rental item별로 Charge 계산
    - calculatePoint() : Rental item 별로 Point 계산
    - printCouponInfo() : total point에 따라 쿠폰 정보를 출력

--- Customer.java
+++ Customer.java
@@ -41,46 +41,60 @@ public class Customer {
 		int totalPoint = 0;
 
 		for (Rental each : rentals) {
-			double eachCharge = 0;
-			int eachPoint = 0 ;
+			double eachCharge = calculateChage(each);
+			int eachPoint = calculatePoint(each);
 			int daysRented = each.getDaysRented();
-
-			switch (each.getVideo().getPriceCode()) {
-				case Video.REGULAR:
-					eachCharge += 2;
-					if (daysRented > 2)
-						eachCharge += (daysRented - 2) * 1.5;
-					break;
-				case Video.NEW_RELEASE:
-					eachCharge = daysRented * 3;
-					break;
-			}
-
-			eachPoint++;
-
-			if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) )
-				eachPoint++;
-
-			if ( daysRented > each.getDaysRentedLimit() )
-				eachPoint -= Math.min(eachPoint, each.getVideo().getLateReturnPointPenalty()) ;
 
 			result += "\t" + each.getVideo().getTitle() + "\tDays rented: " + daysRented + "\tCharge: " + eachCharge
 					+ "\tPoint: " + eachPoint + "\n";
-
-			totalCharge += eachCharge;
-
 			totalPoint += eachPoint ;
+			totalCharge += eachCharge;
 		}
 
+		printCouponInfo(totalPoint);
+
 		result += "Total charge: " + totalCharge + "\tTotal Point:" + totalPoint + "\n";
+		return result ;
+	}
+
+	double calculateChage(Rental each) {
+		double eachCharge = 0;
+		int daysRented = each.getDaysRented();
+
+		switch (each.getVideo().getPriceCode()) {
+			case Video.REGULAR:
+				eachCharge += 2;
+				if (daysRented > 2)
+					eachCharge += (daysRented - 2) * 1.5;
+				break;
+			case Video.NEW_RELEASE:
+				eachCharge = daysRented * 3;
+				break;
+		}
+		return eachCharge;
+	}
+
+	int calculatePoint(Rental each) {
+		int eachPoint = 0 ;
+		int daysRented = each.getDaysRented();
+		eachPoint++;
+
+		if ((each.getVideo().getPriceCode() == Video.NEW_RELEASE) )
+			eachPoint++;
+
+		if ( daysRented > each.getDaysRentedLimit() )
+			eachPoint -= Math.min(eachPoint, each.getVideo().getLateReturnPointPenalty()) ;
 
 
+		return eachPoint;
+	}
+
+	void printCouponInfo(int totalPoint) {
 		if ( totalPoint >= 10 ) {
 			System.out.println("Congrat! You earned one free coupon");
 		}
 		if ( totalPoint >= 30 ) {
 			System.out.println("Congrat! You earned two free coupon");
 		}
-		return result ;
 	}
 }
